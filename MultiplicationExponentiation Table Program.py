01706ebadbc0af2d4bc749ee4e3f57d8e542b979
salah khalil <31698436+salahkh98@users.noreply.github.com>
2021-01-22 20:42:54 +0200

    commit 5
    Hello world

---
+++ MultiplicationExponentiation Table Program.py
@@ -0,0 +1,35 @@
+print("welcom to the Multiplication/Exponent taple app")
+
+inp_name = input("what is your name: ")
+
+inp_num = float(input("what numper would you like to work with: "))
+
+print("Multiplication table for " , inp_num)
+
+print("1.0 *" , inp_num , " = " , round(1 * inp_num, 4))
+print("2.0 *" , inp_num , " = " , round(2 * inp_num, 4))
+print("3.0 *" , inp_num , " = " , round(3 * inp_num, 4))
+print("4.0 *" , inp_num , " = " , round(4 * inp_num, 4))
+print("5.0 *" , inp_num , " = " , round(5 * inp_num, 4))
+print("6.0 *" , inp_num , " = " , round(6 * inp_num, 4))
+print("7.0 *" , inp_num , " = " , round(7 * inp_num ,4))
+print("8.0 *" , inp_num , " = " , round(8 * inp_num), 4)
+print("9.0 *" , inp_num , " = " , round(9 * inp_num, 4))
+print("10.0 *" , inp_num , " = " , round(10 * inp_num, 4))
+
+
+print("\nExponent table for " , inp_num)
+print(inp_num , "** 1" , " = " , round(inp_num ** 1, 4))
+print(inp_num , "** 2" , " = " , round(inp_num ** 2, 4))
+print(inp_num , "** 3" , " = " , round(inp_num ** 3, 4))
+print(inp_num , "** 4" , " = " , round(inp_num ** 4, 4))
+print(inp_num , "** 5" , " = " , round(inp_num ** 5, 4))
+print(inp_num , "** 6" , " = " , round(inp_num ** 6, 4))
+print(inp_num , "** 7" , " = " , round(inp_num ** 7 , 4))
+print(inp_num , "** 8" , " = " , round(inp_num ** 8, 4))
+print(inp_num ,"** 9" , " = " , round(inp_num ** 9, 4))
+print(inp_num , "** 10" , " = " , round(inp_num ** 10, 4))
+
+
+print("\nthank you",inp_name)
+
